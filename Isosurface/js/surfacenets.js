a8d684e4e2ec3e6367b608140b876253af0f4cb3
Mikola Lysenko <mikola@bottleneck.(none)>
2012-07-13 08:00:59 +0200

    Added some comments

--- Isosurface/js/surfacenets.js
+++ Isosurface/js/surfacenets.js
@@ -15,6 +15,9 @@ var SurfaceNets = (function() {
 var cube_edges = new Int32Array(24)
   , edge_table = new Int32Array(256);
 (function() {
+
+  //Initialize the cube_edges table
+  // This is just the vertex number of each cube
   var k = 0;
   for(var i=0; i<8; ++i) {
     for(var j=1; j<=4; j<<=1) {
@@ -26,6 +29,9 @@ var cube_edges = new Int32Array(24)
     }
   }
 
+  //Initialize the intersection table.
+  //  This is 2^(cube configuration) ->  2^(edge configuration) map
+  //  There is one entry for each possible intersection configuration, and the output is a 12-bit vector enumerating all edges intersecting the cube.
   for(var i=0; i<256; ++i) {
     var em = 0;
     for(var j=0; j<24; j+=2) {
@@ -57,11 +63,17 @@ return function(data, dims) {
   
   //March over the voxel grid
   for(x[2]=0; x[2]<dims[2]-1; ++x[2], n+=dims[0], buf_no ^= 1, R[2]=-R[2]) {
+  
+    //m is the pointer into the buffer we are going to use.  
+    //This is slightly obtuse because javascript does not have good support for packed data structures, so we must use typed arrays :(
+    //The contents of the buffer will be the indices of the vertices on the previous x/y slice of the volume
     var m = 1 + (dims[0]+1) * (1 + buf_no * (dims[1]+1));
+    
     for(x[1]=0; x[1]<dims[1]-1; ++x[1], ++n, m+=2)
     for(x[0]=0; x[0]<dims[0]-1; ++x[0], ++n, ++m) {
     
-      //Read in field values
+      //Read in 8 field values around this vertex and store them in an array
+      //Also calculate 8-bit mask, like in marching cubes, so we can speed up sign checks later
       var mask = 0, g = 0, idx = n;
       for(var k=0; k<2; ++k, idx += dims[0]*(dims[1]-2))
       for(var j=0; j<2; ++j, idx += dims[0]-2)      
@@ -71,29 +83,38 @@ return function(data, dims) {
         mask |= (p < 0) ? (1<<g) : 0;
       }
       
-      //Check edge mask
+      //Check for early termination if cell does not intersect boundary
       if(mask === 0 || mask === 0xff) {
         continue;
       }
-      var edge_mask = edge_table[mask];
       
       //Sum up edge intersections
-      var v = [0.0,0.0,0.0], e_count = 0;
+      var edge_mask = edge_table[mask]
+        , v = [0.0,0.0,0.0]
+        , e_count = 0;
+        
+      //For every edge of the cube...
       for(var i=0; i<12; ++i) {
+      
+        //Use edge mask to check if it is crossed
         if(!(edge_mask & (1<<i))) {
           continue;
         }
+        
+        //Increment number of edges
         ++e_count;
-        var e0 = cube_edges[ i<<1 ]
+        var e0 = cube_edges[ i<<1 ]       //Unpack vertices
           , e1 = cube_edges[(i<<1)+1]
-          , g0 = grid[e0]
+          , g0 = grid[e0]                 //Unpack grid values
           , g1 = grid[e1]
-          , t  = g0 - g1;
+          , t  = g0 - g1;                 //Compute point of intersection
         if(Math.abs(t) > 1e-6) {
           t = g0 / t;
         } else {
           continue;
         }
+        
+        //Find intersection vertex and sum
         for(var j=0, k=1; j<3; ++j, k<<=1) {
           var a = e0 & k
             , b = e1 & k;
@@ -110,21 +131,32 @@ return function(data, dims) {
       for(var i=0; i<3; ++i) {
         v[i] = x[i] + s * v[i];
       }
+      
+      //Add vertex to buffer, store pointer to vertex index in buffer
       buffer[m] = vertices.length;
       vertices.push(v);
       
-      //Connect faces together
+      //Now we need to add faces together, to do this we just loop over 3 basis components
       for(var i=0; i<3; ++i) {
+        //The first three entries of the edge_mask count the crossings along the edge
         if(!(edge_mask & (1<<i)) ) {
           continue;
         }
+        
+        // i = axes we are point along.  iu, iv = orthogonal axes
         var iu = (i+1)%3
-          , iv = (i+2)%3
+          , iv = (i+2)%3;
+          
+        //If we are on a boundary, skip it
         if(x[iu] === 0 || x[iv] === 0) {
           continue;
         }
+        
+        //Otherwise, look up adjacent edges in buffer
         var du = R[iu]
           , dv = R[iv];
+        
+        //Remember to flip orientation depending on the sign of the corner.
         if(mask & 1) {
           faces.push([buffer[m], buffer[m-du], buffer[m-du-dv], buffer[m-dv]]);
         } else {
@@ -134,6 +166,7 @@ return function(data, dims) {
     }
   }
   
+  //All done!  Return the result
   return { vertices: vertices, faces: faces };
 };
 })();
